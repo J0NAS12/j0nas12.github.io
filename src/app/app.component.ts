365945a8513b876f7e8c004449284494bc75e202
J0NAS12 <jurasekjonas12@gmail.com>
2026-02-08 23:26:04 +0100

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit , ViewChild, ElementRef} from "@angular/core";
+import { Component, OnInit, ViewChild, ElementRef } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
 
 @Component({
@@ -11,18 +11,16 @@ import { RouterOutlet } from "@angular/router";
 export class AppComponent implements OnInit {
   title = "homepage";
 
-  @ViewChild('rendererCanvas') canvasRef!: ElementRef<HTMLCanvasElement>;
+  @ViewChild("rendererCanvas") canvasRef!: ElementRef<HTMLCanvasElement>;
 
-    private ctx!: CanvasRenderingContext2D;
-    private animationId!: number;
+  private ctx!: CanvasRenderingContext2D;
+  private animationId!: number;
 
-    // State variables
-    private x = 95;
-    private velocity = 3;
-    private radius = 40;
-  constructor() { }
-
-
+  // State variables
+  private x = 95;
+  private velocity = 3;
+  private radius = 40;
+  constructor() {}
 
   ngOnInit(): void {
     const canvas = <HTMLCanvasElement>document.getElementById("canvas");
@@ -37,30 +35,28 @@ export class AppComponent implements OnInit {
       ctx.stroke();
     }
 
-    private animate = () => {
-        const canvas = this.canvasRef.nativeElement;
+    let animate = () => {
+      const canvas = this.canvasRef.nativeElement;
 
-        // Clear, Draw, and Update Logic
-        this.ctx.clearRect(0, 0, canvas.width, canvas.height);
+      // Clear, Draw, and Update Logic
+      this.ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-        this.ctx.beginPath();
-        this.ctx.arc(this.x, 50, this.radius, 0, 2 * Math.PI);
-        this.ctx.fillStyle = "red";
-        this.ctx.fill();
-        this.ctx.lineWidth = 4;
-        this.ctx.strokeStyle = "blue";
-        this.ctx.stroke();
+      this.ctx.beginPath();
+      this.ctx.arc(this.x, 50, this.radius, 0, 2 * Math.PI);
+      this.ctx.fillStyle = "red";
+      this.ctx.fill();
+      this.ctx.lineWidth = 4;
+      this.ctx.strokeStyle = "blue";
+      this.ctx.stroke();
 
-        this.x += this.velocity;
+      this.x += this.velocity;
 
-        // Bounce logic
-        if (this.x + this.radius > canvas.width || this.x - this.radius < 0) {
-          this.velocity *= -1;
-        }
-
-        this.animationId = requestAnimationFrame(this.animate);
+      // Bounce logic
+      if (this.x + this.radius > canvas.width || this.x - this.radius < 0) {
+        this.velocity *= -1;
       }
 
-
+      this.animationId = requestAnimationFrame(this.animate);
+    };
   }
 }
